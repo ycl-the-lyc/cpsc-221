4748133012d1ccf0e2557ff52739527116416408
ycl-the-lyc <steve.lyc.2980@gmail.com>
2026-01-07 13:08:11 -0800

    init

---
+++ lab-0/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "CPSC Lab Intro Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              gcc
+              gnumake
+              
+              libpng
+              zlib
+            ];
+
+            shellHook = ''
+              echo "--- Lab Intro Shell Loaded ---"
+            '';
+          };
+        });
+    };
+}
+
