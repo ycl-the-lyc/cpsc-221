ef1eae0c21c7da6e1e2352f8f953ca00d9bb79f8
ycl-the-lyc <steve.lyc.2980@gmail.com>
2026-01-12 14:03:34 -0800

    fix clangd file not found

--- lab-0/flake.nix
+++ lab-0/flake.nix
@@ -5,32 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    {
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              gcc
-              gnumake
-              
-              libpng
-              zlib
-            ];
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          gcc
+          gnumake
+          gdb
+          bear
+        ];
+        buildInputs = with pkgs; [
+          libpng
+          zlib
+        ];
 
-            shellHook = ''
-              echo "--- Lab Intro Shell Loaded ---"
-            '';
-          };
-        });
-    };
+        shellHook = ''
+          export CLANGD_FLAGS="--query-driver=${pkgs.gcc}/bin/g++,${pkgs.clang}/bin/clang++"
+          bear -- make --always-make --keep-going --silent
+          make clean
+          echo "--- Lab Intro Shell Loaded ---"
+        '';
+      };
+    });
+  };
 }
-
