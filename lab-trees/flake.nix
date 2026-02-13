e83a1c91514eb8dba03b92c57e94357a8ca1d623
ycl-the-lyc <steve.lyc.2980@gmail.com>
2026-02-13 10:26:00 -0800

    lab trees

---
+++ lab-trees/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "CPSC 221 Lab Trees Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      env = pkgs.stdenv;
+      mkbear = pkgs.writeShellScriptBin "mkbear" "bear -- make --always-make --keep-going --silent";
+    in {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          clang
+          gnumake
+          (
+            if env.isDarwin
+            then lldb
+            else gdb
+          )
+          bear
+          mkbear
+          (
+            if env.isLinux
+            then valgrind-light
+            else null
+          )
+        ];
+        buildInputs = with pkgs; [
+        ];
+
+        shellHook = ''
+          export CLANGD_FLAGS="--query-driver=${pkgs.clang}/bin/clang++"
+        '';
+      };
+    });
+  };
+}
